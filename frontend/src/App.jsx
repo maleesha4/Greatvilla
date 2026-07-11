f00e4d3461895fab3bf5c6c960feb1d0855306e2
adithaanusara <adithaanusara31@gmail.com>
2026-07-11 10:07:11 +0530

    folder structure create

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import Navbar from "./components/Navbar/Navbar.jsx";
+import Home from "./pages/Home/Home.jsx";
+
+
+function App() {
+
+  return (
+    <>
+      <Navbar />
+      <Home />
+    </>
+  );
+
+}
+
+export default App;
